9686726fda1881bde4fd6601a773086d2333c467
Stephens37 <braydenstephens@protonmail.com>
2024-01-23 09:26:07 -0600

    Add searchability of locations

--- script.js
+++ script.js
@@ -5,24 +5,19 @@ let temp = document.querySelector('#temp')
 let city = document.querySelector('#city')
 let percent = document.querySelector('#percent')
 let speed = document.querySelector('#speed')
+const input = document.querySelector('input')
+const submit = document.querySelector('#submit')
 
-//function location () {
-  /* if the input matches the name of a city - display the info of that city
-  info needed will include:
-  wind
-  temperature
-  sky conditions
-  humidity
-  */
-
-  fetch('https://api.weatherapi.com/v1/current.json?key=7aa10a33b43847a081114832232012&q=london', {mode: 'cors'})
+function locationFunction () {
+  (console.log('hi'))
+  return fetch(`https://api.weatherapi.com/v1/current.json?key=7aa10a33b43847a081114832232012&q=${input.value}`, { mode: 'cors' })
     .then(function (response) {
       return (response.json())
     })
     /*.then(function (response) {
       console.log(response)
     })*/
-      .then (function (response) {
+    .then(function (response) {
       let sky = document.querySelector('#sky')
       sky.src = response.current.condition.icon
       let temp = document.querySelector('#temp')
@@ -34,28 +29,12 @@ let speed = document.querySelector('#speed')
       let speed = document.querySelector('#speed')
       speed.innerText = response.current.wind_kph
     })
-    /*.then (function (response) {
-      let temp = document.querySelector('#temp')
-      temp.innerText = response.current.temp_c
-    })
-    .then(function (response) {
-      let city = document.querySelector('#city')
-      city.innerText = response.location.name
-    })
-    .then(function (response) {
-      let percent = document.querySelector('#percent')
-      percent.innerText = response.current.humidity
-    })
-    .then(function (response) {
-      let speed = document.querySelector('#speed')
-      speed.innerText = response.current.wind_kph
-    })
     .catch(function (err) {
-      throw err
+      console.log(err)
+      console.log('error')
     })
-//}
+}
 
 // const submit = document.querySelector('#submit')
 
-//location()
-*/
+submit.addEventListener('click', locationFunction)
